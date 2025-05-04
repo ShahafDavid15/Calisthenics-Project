d7be9ef65717ea8c2e319ab8964fb30b4e7e0478
ShahafDavid15 <shahaf.david11@gmail.com>
2025-05-04 21:23:03 +0300

    Add files via upload

---
+++ backend/dbCalisthenics.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS `article_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Select database
+USE `membership`;
+USE `users`;
+USE `user_membership`;
+USE `workout`;
+USE `workout_exercises`;
+USE `workout_registrations`;
